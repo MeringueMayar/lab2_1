673b1fffa8308efafa0d3fa5af42b7686b07d965
Mayar Karkout <204641@edu.p.lodz.pl>
2018-03-20 18:22:10 +0100

    testy_v1 Unit test for BinarySearch ready

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -43,22 +43,35 @@ public class BinarySearchTest {
     @Test
     public void testSearch() {
         System.out.println("search");
-        int key = 2;
-        int[] seq = new int[3];
-        seq[0] = 1;
-        seq[1] = 2;
-        seq[2] = 5;
+        int key = 6;
+        int[] seq = new int[4];
+        seq[0] = 6;
+        seq[1] = 7;
+        seq[2] = 9;
+        seq[3] = 22;
         
-        int asc = 1;
+        boolean asc = true;
+        boolean elementFound = false;
         
         assert seq.length > 0;
-        for (int i = 1; i < seq.length; i++){
-            if (seq[i] <= seq[i-1]){
-                asc = 0;
-                break;
+        System.out.println("Sequence is longer than 0.");
+        if (seq.length > 1){
+            for (int i = 1; i < seq.length; i++){
+                if (seq[i] <= seq[i-1]){
+                    asc = false;
+                    break;
+                }
             }
+        }    
+        assertEquals(asc, true);
+        System.out.println("Sequence is ascending");
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        int pos = searchResult.getPosition();
+        if (searchResult.isFound()== true && seq[pos-1] == key ){
+            elementFound = true;
         }
-        assertEquals(asc, 1);
+        assertEquals(elementFound, true);
+        System.out.println("Element found.");
     }
     
 }
